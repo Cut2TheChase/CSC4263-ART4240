77ae6b9ed77c2baaab39e3c1a4cf50d7d38dd4a7
Michael Marchese <michmarchese@gmail.com>
2017-03-02 21:38:58 -0600

    make successive camera swaps script
    This script will be used on all triggers after the initial trigger

---
+++ Assets/camera Tester/camChange.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camChange : MonoBehaviour {
+
+	public Camera cam1;
+	public Camera cam2;
+	public Camera cam3;
+	public GameObject player;
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		cam1.enabled = false;
+		cam2.enabled = true;
+		cam3.enabled = false;
+	}
+
+}
