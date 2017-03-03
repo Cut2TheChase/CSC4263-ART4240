36e0b0d4334fa6c32573ead83db7a715936ed036
Michael Marchese <michmarchese@gmail.com>
2017-03-02 21:37:03 -0600

    make camera follow script
    script that sets the main camera to follow the player

---
+++ Assets/camera Tester/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+	public GameObject player;
+
+	Vector3 offset;  // create this object to edit the generic offset of the camera
+
+	void Start () 
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+
+	void LateUpdate ()   // Late update function is another update function that happens after every other update function has been called
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
+
