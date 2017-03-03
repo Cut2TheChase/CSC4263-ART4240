72b90fb6c9670e6e3caacdc85b4bca0fdcc88a79
Michael Marchese <michmarchese@gmail.com>
2017-03-02 21:40:55 -0600

    make/steal Kooima's throttle script
    Script attached to car prefab allows car to move when left and right
    arrow keys are pressed.
    
    Small change to original script.  Added public variable power to allow
    change of speed throttle from the unity hud.

---
+++ Assets/camera Tester/throttle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throttle : MonoBehaviour {
+
+	WheelJoint2D[] wheelJoints;
+	JointMotor2D jointMotor;
+	public int power;
+
+	void Start () 
+	{
+		wheelJoints = gameObject.GetComponents<WheelJoint2D> ();
+		jointMotor = wheelJoints[0].motor;
+
+	}
+	
+
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			jointMotor.motorSpeed = -power;
+
+				
+		} 
+		else if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			jointMotor.motorSpeed = power;
+		
+		}
+		else 
+		{
+			jointMotor.motorSpeed = 0;
+
+		}
+		wheelJoints[0].motor = jointMotor;
+	}
+}
