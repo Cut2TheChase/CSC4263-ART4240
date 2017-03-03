e79ab1c9942b7325a877a617edf87bf2d24d8087
Michael Marchese <michmarchese@gmail.com>
2017-03-02 21:38:02 -0600

    make camera initial swap script

---
+++ Assets/camera Tester/cameraSwap.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSwap : MonoBehaviour {
+
+
+	public Camera cam1;
+	public Camera cam2;
+	public Camera cam3;
+	public GameObject player;
+	public bool change1 = false;
+	public bool change2 = true;
+	public bool change3 = false;
+
+	void Start()
+	{
+		cam1.enabled = true;
+		cam2.enabled = false;
+		cam3.enabled = false;
+	}
+
+
+
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		
+		cam1.enabled = false;
+		cam2.enabled = true;
+		cam3.enabled = false;
+
+	}
+
+}
