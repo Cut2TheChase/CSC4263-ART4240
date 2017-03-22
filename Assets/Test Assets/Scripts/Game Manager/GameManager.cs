208388a6f70a1b0c8384903507676d449139bfb3
Cut2TheChase <steven.chase.bernard@gmail.com>
2017-03-21 20:14:22 -0500

    Create Game Manager
    Created a simple game state manager script which is instantiated by the
    loader script, and will not be destroyed when switching scenes

---
+++ Assets/Test Assets/Scripts/Game Manager/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+
+	// Use this for initialization
+	void Awake () {
+
+		//if there is no other instance, this is the instance
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+
+		DontDestroyOnLoad(gameObject);
+	}
+		
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test Assets/Scripts/Game Manager/Loader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject gameManager;
+
+	void Awake () {
+		//if there is no GameManager object, make one
+		if (GameManager.instance == null)
+			Instantiate (gameManager);
+	}
+
+}
