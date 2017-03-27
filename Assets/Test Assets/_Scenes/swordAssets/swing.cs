c4ab76306aafbbacf510f2d67a357a5f334934c1
Michael Marchese <michmarchese@gmail.com>
2017-03-27 11:19:15 -0500

    fabricate character weapon (sword)
    Created the assets and scripts for the characters weapon.
    
    Bundled these into a prefab and created a scene for ease of testing.

---
+++ Assets/Test Assets/_Scenes/swordAssets/swing.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swing : MonoBehaviour {
+
+	public float swingTime;
+	public float swingSpeed;
+	HingeJoint2D hinge;
+	JointMotor2D jointMotor;
+	private bool swung = false;
+	public float swingDelay;
+
+	void Start () {
+		hinge = gameObject.GetComponent<HingeJoint2D>();	
+		jointMotor = hinge.motor;
+		jointMotor.motorSpeed = -2000;
+		swingDelay = swingTime + 0.2F;
+
+
+	}
+	IEnumerator ExecuteTime(float time)
+	{
+		yield return new WaitForSeconds(time);
+		jointMotor.motorSpeed = -2000; 
+
+
+	}
+	IEnumerator DelayInput(float time)
+	{
+		yield return new WaitForSeconds(time);
+		swung = false; 
+
+
+	}
+	void Update () {
+		if (Input.GetKey (KeyCode.Y)) 
+		{
+			if (swung == false) {	
+				jointMotor.motorSpeed = swingSpeed;
+				StartCoroutine (ExecuteTime (swingTime));
+				swung = true;
+				StartCoroutine (DelayInput (swingDelay));
+			}
+
+
+
+		} 
+
+		hinge.motor = jointMotor;
+	}
+}
---
+++ Assets/Test Assets/_Scenes/swordAssets/swing2.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swing2 : MonoBehaviour {
+
+
+	public float thrustTime;
+	public int thrustSpeed;
+	SliderJoint2D slider;
+	JointMotor2D sliderMotor;
+	private bool swung = false;
+	public float thrustDelay;
+	public swing sword;
+
+	void Start () {
+		
+
+		slider = gameObject.GetComponent<SliderJoint2D>();	
+		sliderMotor = slider.motor;
+		sliderMotor.motorSpeed = -2000;
+		thrustDelay = sword.swingDelay;
+
+	}
+
+	IEnumerator ExecuteTime(float time)
+	{
+		yield return new WaitForSeconds(time);
+		sliderMotor.motorSpeed = -2000; 
+
+
+	}
+	IEnumerator DelayInput(float time)
+	{
+		yield return new WaitForSeconds(time);
+		swung = false; 
+
+
+	}
+	void Update () {
+		thrustDelay = sword.swingDelay;
+
+		if (Input.GetKey (KeyCode.Y)) 
+		{
+			if (swung == false) {
+				sliderMotor.motorSpeed = thrustSpeed;
+				StartCoroutine (ExecuteTime (thrustTime));
+				swung = true;
+				StartCoroutine (DelayInput (thrustDelay));
+			}
+		} 
+
+
+		slider.motor = sliderMotor;
+	}
+}
