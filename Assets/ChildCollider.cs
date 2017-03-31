f9d7bcb3884bf6b2900011a23a976131d9f4451a
Cut2TheChase <steven.chase.bernard@gmail.com>
2017-03-30 19:34:45 -0500

    Have Scene Saved with Child Collider (Not Functional)
    Forgot to save my scene, I just added in a child collider for the
    character as well, however its not fully functional

---
+++ Assets/ChildCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Debug.Log ("Hey now, you're a rockstar");
+		this.GetComponent<Collider2D> ().attachedRigidbody.SendMessage ("OnTriggerEnter2D", other);
+	}
+
+}
