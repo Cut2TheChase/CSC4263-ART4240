6f4efc58985fdb8ff9d914d7a40947e277b9d6e8
Cut2TheChase <steven.chase.bernard@gmail.com>
2017-04-01 18:21:32 -0500

    New Background Art Assets and Parallax
    Added new background art assets and a parallax script

---
+++ Assets/Test Assets/Scripts/Movements/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	public float parallaxSpeed;
+	private float lastCameraX;
+
+	// Use this for initialization
+	void Start () {
+		lastCameraX = Camera.main.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaX = Camera.main.transform.position.x - lastCameraX;
+		lastCameraX = Camera.main.transform.position.x;
+
+		transform.position += Vector3.right * (deltaX * parallaxSpeed);
+	}
+}
