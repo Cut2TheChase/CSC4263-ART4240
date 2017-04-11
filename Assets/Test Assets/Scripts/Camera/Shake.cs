9db8fca56186acbb2680f6d23bc8ff2336b58a50
Michael Marchese <michmarchese@gmail.com>
2017-04-11 13:03:37 -0500

    fix camera clamp issue with boss fight
    camera no longer shows out of play area when boss’s hands slam the
    ground

--- Assets/Test Assets/Scripts/Camera/Shake.cs
+++ Assets/Test Assets/Scripts/Camera/Shake.cs
@@ -7,6 +7,23 @@ public class Shake : MonoBehaviour {
 	public float duration;
 	public float magnitude;
 
+	public BoxCollider2D Bounds;
+	public EdgeCollider2D Ground;
+
+	private Vector3 
+	_min, 
+	_max,
+	_minY,
+	_maxY;
+
+	public void Start()
+	{
+		_min = Bounds.bounds.min;
+		_max = Bounds.bounds.max;
+		_minY = Ground.bounds.min;
+		_maxY = Ground.bounds.max;
+	}
+
 	//Starts the shaking
 	public void Shaker(){
 		StartCoroutine (Shakey());
@@ -19,6 +36,15 @@ public class Shake : MonoBehaviour {
 		Vector3 originalCamPos = Camera.main.transform.position;
 
 		while (elapsed < duration) {
+			var x_pos = transform.position.x;
+			var y_pos = transform.position.y;
+
+			var cameraHalfWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+
+
+
+
+
 			elapsed += Time.deltaTime;
 			float percentComplete = elapsed / duration;
 
@@ -28,14 +54,32 @@ public class Shake : MonoBehaviour {
 			float x = Random.value * 2.0f - 1.0f;
 			float y = Random.value * 2.0f - 1.0f;
 
+
+
 			x *= magnitude * damper;
 			y *= magnitude * damper;
 
-			Camera.main.transform.position = new Vector3 (x, y, originalCamPos.z);
+			x_pos = Mathf.Clamp (x, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+			y_pos = Mathf.Clamp (y, _minY.y + GetComponent<Camera> ().orthographicSize, _max.y - GetComponent<Camera> ().orthographicSize);
+
+			Camera.main.transform.position = new Vector3 (x_pos, y_pos, transform.position.z);//new Vector3 (x, y, originalCamPos.z);
 
 			yield return null;
 		}
 
 		Camera.main.transform.position = originalCamPos;
+	}
+
+	public void Update()
+	{
+
+		var x_pos = transform.position.x;
+		var y_pos = transform.position.y;
+
+		var cameraHalfWidth = GetComponent<Camera>().orthographicSize * ((float)Screen.width / Screen.height);
+
+		x_pos = Mathf.Clamp (x_pos, _min.x + cameraHalfWidth, _max.x - cameraHalfWidth);
+		y_pos = Mathf.Clamp (y_pos, _minY.y + GetComponent<Camera> ().orthographicSize, _max.y - GetComponent<Camera> ().orthographicSize);
+
 	}
 }
