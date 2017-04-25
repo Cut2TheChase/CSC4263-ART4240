dd20a2a02d38aed72087bd56cadc51990c45c40f
mightyadamyoung <mauco29@lsu.edu>
2017-04-25 00:21:34 -0500

    added header

--- Assets/Test Assets/Scripts/Game Manager/SoundKeeper.cs
+++ Assets/Test Assets/Scripts/Game Manager/SoundKeeper.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿///Uptown Pigeon Gaming
+///Project Fugue
+///CSC4263-ART4240
+///Dr. Robert Kooima
+///Code Description -- A code that manages the audio player for the main level theme.
+///Author -- Mitchell Aucoin
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 public class SoundKeeper : MonoBehaviour
