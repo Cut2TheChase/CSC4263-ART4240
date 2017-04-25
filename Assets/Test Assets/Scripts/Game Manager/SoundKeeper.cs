cbc42339ca004cde1e5d38ca3eff0ba09b21868d
mightyadamyoung <mauco29@lsu.edu>
2017-04-25 00:20:19 -0500

    Made a sound manager

---
+++ Assets/Test Assets/Scripts/Game Manager/SoundKeeper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SoundKeeper : MonoBehaviour
+{
+    private static SoundKeeper instance = null;
+    public static SoundKeeper Instance
+    {
+        get { return instance; }
+    }
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
