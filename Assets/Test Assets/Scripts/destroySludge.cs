706d35258791b4c4a8cb54099f1781422d5eafbe
Cut2TheChase <steven.chase.bernard@gmail.com>
2017-04-25 01:11:56 -0500

    Scene 2 fixes
    Yay!

--- Assets/Test Assets/Scripts/destroySludge.cs
+++ Assets/Test Assets/Scripts/destroySludge.cs
@@ -8,10 +8,17 @@ public class destroySludge : MonoBehaviour {
 	public GameObject sludge;
 	public GameObject rangedEnemy;
 
+	private SpriteRenderer fader;
+	private float t;
+	private float durationOut = 1.5f;
+	private bool gotStart = false;
+	private float startTime;
+
+	private bool destroyWall = false;
 
 	// Use this for initialization
 	void Start () {
-		
+		fader = sludge.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -20,6 +27,19 @@ public class destroySludge : MonoBehaviour {
 
 		if (rangedEnemy.GetComponent<RangedEnemyHealth> ().health < 1) {
 			Destroy(sludge.GetComponent<SludgeSpawn>());
+			if (gotStart == false) {
+				startTime = Time.time;
+				gotStart = true;
+			}
+			t = (Time.time - startTime) / durationOut;
+			fader.color = new Color(fader.color.r,fader.color.g,fader.color.b,Mathf.SmoothStep(1,0,t));
+
+			if (destroyWall == false) {
+				
+				Destroy (GameObject.Find("Wall"));
+				destroyWall = true;
+
+			}
 		}
 		
 	}
