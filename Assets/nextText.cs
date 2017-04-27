14175a6b2a69e23f63c637ae9d28ccbb2e16b6be
Michael Marchese <michmarchese@gmail.com>
2017-04-26 21:36:47 -0500

    Revert "Revert "Revert "Revert "Merge remote-tracking branch 'origin/master'""""
    This reverts commit 7dd12f81cbd61c6eec9a01ae621acad1b344cf0b.

---
+++ Assets/nextText.cs
@@ -0,0 +1,19 @@
+﻿///Uptown Pigeon Gaming
+///Project Fugue
+///CSC4263-ART4240
+///Dr. Robert Kooima
+///Code Description -- Holds a reference, if any, to the proceeding text for a cutscene event. Also hides cutscene text on the Canvas by default.
+///Author -- Jonathan Nguyen
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nextText : MonoBehaviour
+{
+    public UnityEngine.UI.Text next;
+    
+    void Start()
+    {
+        gameObject.gameObject.SetActive(false);
+    }
+}
